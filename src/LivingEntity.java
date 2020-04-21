ca12ce971c08fd1f166ba3fb64783c70f8b72e2a
Tomasz Gardocki <tomasz.gardocki.stud@pw.edu.pl>
2020-04-21 15:53:03 +0200

    Poprawa klas Entity i LivingEntity, dodanie klas Pacman i CollectableEntity

--- src/LivingEntity.java
+++ src/LivingEntity.java
@@ -1,20 +1,20 @@
 import java.awt.Rectangle;
+import java.awt.Graphics;
 
-public abstract class LivingEntity implements Entity {
+public abstract class LivingEntity extends Entity {
+    boolean alive;
+    double speed; // wyrażona w pikselach na klatkę
+    enum Direction {UP, DOWN, LEFT, RIGHT}
+    Direction direction;
 
-    private boolean alive;
-    private double speed; // wyrażona w pikselach na klatkę
-    private enum Direction {UP, DOWN, LEFT, RIGHT}
-    private Direction direction;
-    private boolean[][] board;
+    boolean[][] board;
 
-
-    public abstract void tick();
-
+    // przekazanie położenie ścian do tej klasy
     public void pushBoard(boolean[][] board) {
         this.board = board;
     }
 
+    // zwraca, czy postać może się poruszyć
     public boolean canMove() {
         final boolean BOARD_WALL = true;
 
@@ -38,27 +38,32 @@ public abstract class LivingEntity implements Entity {
         // jeśli któryś z kątów Entity w następnej klatce
         // znalazłby się w ścianie, to Entity nie może
         // się poruszyć
-        if(board[boundsNext.y / 20][boundsNext.x / 20] == BOARD_WALL)
+        if(board[toCells(boundsNext.y)][toCells(boundsNext.x)] == BOARD_WALL)
             return false;
-        if(board[(boundsNext.y + boundsNext.height) / 20][boundsNext.x / 20] == BOARD_WALL)
+        if(board[toCells(boundsNext.y + boundsNext.height)][toCells(boundsNext.x)] == BOARD_WALL)
             return false;
-        if(board[boundsNext.y / 20][(boundsNext.x + boundsNext.width) / 20] == BOARD_WALL)
+        if(board[toCells(boundsNext.y)][toCells(boundsNext.x + boundsNext.width)] == BOARD_WALL)
             return false;
-        if(board[(boundsNext.y + boundsNext.height) / 20][(boundsNext.x + boundsNext.width) / 20] == BOARD_WALL)
+        if(board[toCells(boundsNext.y + boundsNext.height)][toCells(boundsNext.x + boundsNext.width)] == BOARD_WALL)
             return false;
 
         // wszystkie kąty zostały sprawdzone, kolizji nie będzie
         return true;
     }
 
+    // teleportuje postać na wskazane x/y
+    public void teleport(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // zamienia długość z pikseli na komórki
+    int toCells(int value) {
+        return value / 20;
+    }
+
     @Override
-    public void render() {
+    public void render(Graphics g) {
         // TODO: zrobić
     }
-
-    @Override
-    public Rectangle getBounds() {
-        return new Rectangle(x, y, width, height);
-    }
-
 }
---
+++ src/Pacman.java
@@ -0,0 +1,107 @@
+public class Pacman extends LivingEntity {
+
+    private int lives = 3;
+    private int score = 0;
+    private double powerupTimeLeft = 0;
+    private int killedGhostsStreak = 0;
+
+    private final int POWERUP_TIME = 15;
+
+    Ghost[] ghosts;
+    CollectableEntity[][] dots;
+    Fruit[] fruits;
+
+    // przekazanie położenie ścian do tej klasy
+    public void pushGhosts(Ghost[] ghosts) {
+        this.ghosts = ghosts;
+    }
+
+    // przekazanie położenie kulek do tej klasy
+    public void pushDots(CollectableEntity[][] dots) {
+        this.dots = dots;
+    }
+
+    // przekazanie położenie owoców do tej klasy
+    public void pushFruits(Fruit[] fruits) {
+        this.fruits = fruits;
+    }
+
+    // dodaje punkty
+    public void addScore(int value) {
+        score += value;
+    }
+
+    // porusza w ustalonym kierunku
+    public void move() {
+        if(canMove()) {
+            switch(direction) {
+                case UP:
+                    y -= speed;
+                    break;
+                case DOWN:
+                    y += speed;
+                    break;
+                case LEFT:
+                    x -= speed;
+                    break;
+                case RIGHT:
+                    x += speed;
+                    break;
+            }
+        }
+    }
+
+    // nastąpiła kolizja z duchem
+    public void collision(Ghost g) {
+        if(powerupTimeLeft > 0) {
+            addScore(g.points);
+            killedGhostsStreak++;
+            g.die();
+        } else {
+            loseLife();
+        }
+    }
+
+    // zmiana kierunku
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
+    // utrata życia
+    private void loseLife() {
+        killedGhostsStreak = 0;
+        lives--;
+        if(lives > 0) {
+            // TODO: aktualnie respawn jest na środku mapy, ewentualnie zmienić
+            teleport((420 - width) / 2, (460 - height) / 2);
+            // TODO: forEach Ghost teleport na start
+        }
+        // TODO: else koniec gry
+    }
+
+    @Override
+    public void tick() {
+        move();
+
+        if(powerupTimeLeft > 0) {
+            powerupTimeLeft -= (double)1/60;
+        }
+
+        for(Ghost g : ghosts) {
+            if(getBounds().intersects(g.getBounds())) {
+                collision(g);
+            }
+        }
+
+        if(dots[toCells(y)][toCells(x)] != null) {
+            dots[toCells(y)][toCells(x)].pickup(this);
+        }
+
+        for(Fruit f : fruits) {
+            if(getBounds().intersects(f.getBounds())) {
+                f.pickup(this);
+            }
+        }
+    }
+
+}
