2c36a182bc49803684681c0d9476e11679281fd7
Bartosz <barzak200@gmail.com>
2020-05-16 11:43:47 +0200

    Zmiany w render() - scalowanie + w tym jest rzutowanie na int, bo scale jest double

--- src/Dot.java
+++ src/Dot.java
@@ -16,11 +16,12 @@ public class Dot extends CollectableEntity {
 
     @Override
     public void render(Graphics g) {
-        final int size = 7;
+        final int size = (int) (7 * Maze.scale);
 
         if (renderable) {
             g.setColor(Color.ORANGE);
-            g.fillOval(toPixelsX(x) + (Maze.cellSize - size) / 2 + Maze.deltaX, toPixelsY(y) + (Maze.cellSize - size) / 2 + Maze.deltaY, size, size);
+            g.fillOval((int) ((toPixelsX(x) + (Maze.cellSize - size) / 2 + Maze.deltaX) * Maze.scale),
+                    (int) ((toPixelsY(y) + (Maze.cellSize - size) / 2 + Maze.deltaY) * Maze.scale), size, size);
         }
     }
 
--- src/Ghost.java
+++ src/Ghost.java
@@ -145,7 +145,8 @@ public class Ghost extends LivingEntity {
         }
 
         try {
-            g.drawImage(ImageIO.read(new File(imgPath)), this.x + Maze.deltaX, this.y + Maze.deltaY, this.width, this.height, null);
+            g.drawImage(ImageIO.read(new File(imgPath)), (int) ((this.x + Maze.deltaX) * Maze.scale), (int) ((this.y + Maze.deltaY) * Maze.scale),
+                    (int) (this.width * Maze.scale), (int) (this.height * Maze.scale), null);
         } catch (Exception e) {
             e.printStackTrace();
         }
