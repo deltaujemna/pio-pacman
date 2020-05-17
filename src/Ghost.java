39bde64a3ba5d550e2226ae8fe9cfca9ca796e3e
czechoa <andrzej.czechu.ja@gmail.com>
2020-05-17 23:30:09 +0200

    zmienie telportu z kratek na pixele

--- src/Ghost.java
+++ src/Ghost.java
@@ -122,15 +122,15 @@ public class Ghost extends LivingEntity {
                 if(direction != Direction.RIGHT) {
                     direction = Direction.LEFT;
                     if (this.x == 20) {
-                        teleport(toPixelsX(18), toPixelsY(8));
+                        teleport(380, 180);//18 8
                     }
                     return true;
                 }
-            } else if (toCellsX(this.x) > 14) {
+            } else if(this.x > 300) {
                 if(direction != Direction.LEFT) {
                     direction = Direction.RIGHT;
-                    if (toCellsX((this.x)) == 18) {
-                        teleport(toPixelsX(0), toPixelsY(8));
+                    if (this.x == 380) {
+                        teleport(20, 180);//18 8
                     }
                 }
                 return true;
