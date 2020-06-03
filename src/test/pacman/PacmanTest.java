871293169b9e49ba736fa3cedfe1624ff8b7717c
Mateusz Mazewski <mateusz.mazewski.stud@pw.edu.pl>
2020-06-03 20:00:10 +0200

    Dodano test collision_pacmanLosesLifeAndIsStillAlive_pacmanAndGhostsShouldResetPositions() w PacmanTest

--- src/test/pacman/PacmanTest.java
+++ src/test/pacman/PacmanTest.java
@@ -26,6 +26,7 @@ class PacmanTest {
     @Test
     public void collision_pacmanPowerupActive_ghostDies() {
         maze.pacman.activatePowerup();
+        int startLives = maze.pacman.getLives();
         maze.ghosts[0].x = maze.pacman.x;
         maze.ghosts[0].y = maze.pacman.y;
 
@@ -33,10 +34,28 @@ class PacmanTest {
         int finalScore = maze.pacman.getScore();
 
         assertTrue(maze.pacman.alive);
+        assertEquals(startLives, maze.pacman.getLives());
         //Pacman nie rusza się z miejsca, więc zjadł tylko 1 kulkę, zatem >100 pkt musi
         //być za zjedzenie ducha
         assertTrue(finalScore > 100);
         assertFalse(maze.ghosts[0].alive);
+    }
+
+    @Test
+    public void collision_pacmanLosesLifeAndIsStillAlive_pacmanAndGhostsShouldResetPositions() {
+        int startLives = maze.pacman.getLives();
+        maze.pauseLeft = 500;
+        maze.ghosts[0].x = maze.pacman.x;
+        maze.ghosts[0].y = maze.pacman.y;
+
+        maze.pacman.collision(maze.ghosts[0]);
+
+        assertEquals(startLives - 1, maze.pacman.getLives());
+        assertTrue(maze.ghosts[0].alive);
+        assertEquals(maze.pacman.toCells(maze.pacman.startX), maze.pacman.toCells(maze.pacman.x));
+        assertEquals(maze.pacman.toCells(maze.pacman.startY), maze.pacman.toCells(maze.pacman.y));
+        assertEquals(maze.ghosts[0].toCells(maze.pacman.startY), maze.ghosts[0].toCells(maze.pacman.y));
+        assertEquals(maze.ghosts[0].toCells(maze.pacman.startY), maze.ghosts[0].toCells(maze.pacman.y));
     }
 
     @Test
